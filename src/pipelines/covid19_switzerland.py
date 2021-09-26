c3a093cc4997184185d79326cc01012f4d1ff2a1
Nino Müller <n.mueller@substring.ch>
2021-09-26 17:59:18 +0200

    save file

--- src/pipelines/covid19_switzerland.py
+++ src/pipelines/covid19_switzerland.py
@@ -6,7 +6,7 @@ from dagster import pipeline, solid
 @solid
 def covid19_switzerland(context):
     response = requests.get(
-        "https://data.bs.ch/explore/dataset/100077/download/?format=csv&timezone=Europe/Zurich&lang=en&use_labels_for_header=true&csv_separator=%3B")
+        "https://data.bs.ch/explore/dataset/100077/download/?format=csv&timezone=Europe/Zurich&lang=en&use_labels_for_header=true&csv_separator=,")
     lines = response.text.split("\n")
     rows = [row for row in csv.DictReader(lines)]
     context.log.info(f"Found {len(rows)} rows")
